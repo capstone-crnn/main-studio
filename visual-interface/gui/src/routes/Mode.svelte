6b369c6054fb348d52b9e0c75dd7a0561b1a556d
Nour Alsafadi <nouralsafadi@hotmail.com>
2022-02-19 23:04:22 -0500

    [Bug] Fixed routing and SVG linking issues

--- visual-interface/gui/src/routes/Mode.svelte
+++ visual-interface/gui/src/routes/Mode.svelte
@@ -1,30 +1,44 @@
 <script>
 	// Imports
-	import { Link } from "svelte-routing";
+	import { link } from "svelte-spa-router";
 
 </script>
 
-<main>
-	<h1> Mode </h1>
 
-	<div>
-		<h2>Search & Rescue</h2>
-		<p>Use this mode to detect nearby vital signs</p>
-		<Link to="search">
-			<button>
-				<img src="/graphics/human-resources-search-employees.svg" alt="Search" width="64px" /> <!--TODO: Fix images -->
-			</button>
-		</Link>
-	</div>
-
-	<div>
-		<h2>Monitor Vitals</h2>
-		<p>Calibrate device to measure immediate vital signs</p>
-		<Link to="monitor">
-			<button>
-				<img src="graphics/graph-stats-circle.svg" alt="Monitor" width="64px" /> <!--TODO: Fix images -->
-			</button>
-		</Link>
+<main class="container px-20 mx-auto">
+	
+	<div class="grid items-center grid-rows-3 gap-2">
+		<h1 class="font-sans text-2xl font-bold text-center"> 
+			Mode
+		</h1>
+		
+		<div class="grid grid-cols-2 gap-2 place-items-center">
+			<div class="pl-6">
+				<h2 class="text-xl font-bold">
+					Search & Rescue
+				</h2>
+				<p>Use this mode to detect nearby vital signs</p>
+			</div>
+			<a href="/search" use:link>
+				<button class="p-4 rounded-md">
+					<img src="graphics/human-resources-search-employees.svg" alt="Search" width="64px" />
+				</button>
+			</a>
+		</div>
+	
+		<div class="grid grid-cols-2 gap-2 place-items-center">
+			<div class="pl-6">
+				<h2 class="text-xl font-bold">
+					Monitor Vitals
+				</h2>
+				<p>Calibrate device to measure immediate vital signs</p>
+			</div>
+			<a href="/monitor" use:link>
+				<button class="p-4 rounded-md">
+					<img src="graphics/graph-stats-circle.svg" alt="Monitor" width="64px" />
+				</button>
+			</a>
+		</div>
 	</div>
 </main>
 
@@ -32,21 +46,5 @@
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-		/* background-color: #1c2541; */
-	}
-
-	h1 {
-		color: #ff3e00;
-		/* color: #a2d729;
-		text-shadow:1px 1px 10px #c4e675; */
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+	
 </style>
