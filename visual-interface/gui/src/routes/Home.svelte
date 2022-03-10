aef2a840c105c75c7a614965581c8ee21e20861c
Nour Alsafadi <nouralsafadi@hotmail.com>
2022-03-10 12:09:31 -0500

    [Core] Updated homepage with logo

--- visual-interface/gui/src/routes/Home.svelte
+++ visual-interface/gui/src/routes/Home.svelte
@@ -2,28 +2,29 @@
 	// Imports
 	import { link } from "svelte-spa-router";
 
-
-	let version;
-	const get_version = async () => {
-		version = await api.GetVersion();
-	}
 </script>
 
 <main class="container h-full px-20 mx-auto my-auto text-center">
-	<h1 class="py-12 text-3xl">
-		finderFly Logo
-	</h1>
-	<a href="/mode" use:link>
-		<button class="p-2 font-sans text-xl hover:bg-orange-300">
+
+	<div class="pt-12">
+		<img src="graphics/GroupS01_Logo-02.svg" alt="Logo" class="inline m-2 align-middle w-28" />
+		<h1 class="inline py-12 text-6xl align-middle">
+			finderFly
+		</h1>
+	</div>
+	<a href="/mode" use:link class="items-center block pt-6">
+		<button class="px-6 py-4 font-sans text-2xl rounded-lg">
 			START
 		</button>
 	</a>
 </main>
 
 <style global lang="postcss">
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 
-
+	h1 { 
+		color: #F28E44;
+	}
 </style>
