51118de6ffeb6c53cdca3479c9fdc6d5e57988c8
Nour Alsafadi <nouralsafadi@hotmail.com>
2022-03-10 12:10:31 -0500

    [Bug] Updated nav corners with conditional layout

--- visual-interface/gui/src/common/NavCorners.svelte
+++ visual-interface/gui/src/common/NavCorners.svelte
@@ -1,6 +1,12 @@
 <script>
 	// Imports
-	import { link } from "svelte-spa-router";
+	import { afterUpdate } from "svelte";
+	import { link, location } from "svelte-spa-router";
+
+	let currentPath;
+	afterUpdate(() => {
+		currentPath = $location;
+	})
 
 </script>
 
@@ -13,14 +19,32 @@
 	</div>
 </div>
 
+
+<!-- TODO: clean up -->
 <div class="fixed flex flex-wrap justify-between h-full bottom-1">
 	<div class="flex flex-col justify-between">
-		<a href="/mode" class="inline border-b-2 border-r-2 rounded-br-3xl" use:link>
-			<img src="graphics/back.svg" alt="Back" class="w-12 m-2" />
-		</a> <!--TODO: add conditions -->
-		<a href="/" class="inline border-t-2 border-r-2 rounded-tr-3xl" use:link>
-			<img src="graphics/homepage.svg" alt="Home" class="w-12 m-2" />
-		</a>
+		{#if currentPath === "/"}
+			<a href="/" class="inline border-b-2 border-r-2 rounded-br-3xl" use:link on:click={() => window.api.PowerOff()}>
+				<img src="graphics/power-button.svg" alt="Power Off" class="w-12 m-2" />
+			</a>
+			
+		{:else}
+			{#if currentPath === "/mode"}
+				<a href="/" class="inline border-b-2 border-r-2 rounded-br-3xl" use:link>
+					<img src="graphics/back.svg" alt="Back" class="w-12 m-2" />
+				</a>
+				<a href="/" class="inline border-t-2 border-r-2 rounded-tr-3xl" use:link>
+					<img src="graphics/homepage.svg" alt="Home" class="w-12 m-2" />
+				</a>
+			{:else}
+				<a href="/mode" class="inline border-b-2 border-r-2 rounded-br-3xl" use:link>
+					<img src="graphics/back.svg" alt="Back" class="w-12 m-2" />
+				</a>
+				<a href="/" class="inline border-t-2 border-r-2 rounded-tr-3xl" use:link>
+					<img src="graphics/homepage.svg" alt="Home" class="w-12 m-2" />
+				</a>
+			{/if}
+		{/if}
 	</div>
 </div>
 
